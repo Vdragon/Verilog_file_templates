cb3ef05b5339de2cdff056a2d13432cb1ed70547
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-05-23 11:16:45 +0800

    改善表示法

--- module.v
+++ module.v
@@ -6,5 +6,4 @@
 module ();
   output
   input
-
 endmodule
--- testbench.v
+++ testbench.v
@@ -6,7 +6,7 @@
 //時脈頻率
 //`define CLOCK_FREQ 50
 
-module _tb;
+module _tb();
   reg ;
   wire ;
 
@@ -18,15 +18,15 @@ module _tb;
     #`CLOCK_FREQ Clk = !Clk;
 	  end
   */
-  
+
 
   initial
     begin
-	    //初始化    
+	    //初始化
       $dumpfile ("Simulation/_tb.vcd");
       $dumpvars;
       $monitor($time, " ");
-      
+
       //模擬
     end
 endmodule
