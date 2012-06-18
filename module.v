f45362a396ac4e774243905c69608194a3e45c6a
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-06-18 09:56:14 +0800

    增加module檔案功能

--- module.v
+++ module.v
@@ -1,9 +1,16 @@
-/* 模組名稱：
-   著作權宣告：copyright 2012 林博仁(pika1021@gmail.com)
-   */
+// 時間相關設定
 `timescale 1ns / 100ps
 
+/* 模組名稱：
+   著作權宣告：copyright 2012 林博仁(pika1021@gmail.com) */
 module ();
-  output
-  input
+//port 輸出輸入宣告
+  output ;
+  input ;
+
+//port 類型宣告
+	reg ;
+  wire ;
+
+
 endmodule
