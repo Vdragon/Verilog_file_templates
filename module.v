132da0322b820f4130887404dc7244f7ebba914c
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-06-18 10:03:14 +0800

    [module]新增include guard

--- module.v
+++ module.v
@@ -1,16 +1,20 @@
-// 時間相關設定
-`timescale 1ns / 100ps
+//include guard
+`ifndef
+	`define
+	// 時間相關設定
+	`timescale 1ns / 100ps
 
-/* 模組名稱：
-   著作權宣告：copyright 2012 林博仁(pika1021@gmail.com) */
-module ();
-//port 輸出輸入宣告
-  output ;
-  input ;
+	/* 模組名稱：
+		著作權宣告：copyright 2012 林博仁(pika1021@gmail.com) */
+	module ();
+	//port 輸出輸入宣告
+		output ;
+		input ;
 
-//port 類型宣告
-	reg ;
-  wire ;
+	//port 類型宣告
+		reg ;
+		wire ;
 
 
-endmodule
+	endmodule
+`endif
