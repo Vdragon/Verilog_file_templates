1f288ae86734f7e70c7c54b4f8145d5c30e02601
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-06-22 22:47:18 +0800

    [module]改善說明

--- module.v
+++ module.v
@@ -1,7 +1,9 @@
 //include guard
 `ifndef 
 	`define 
-	// 時間相關設定
+	// 指定模擬器的時間單位與精確度
+	// 參考資料 | Reference Data
+	// 		http://www.asic-world.com/verilog/compiler2.html#%60timescale
 	`timescale 1ns / 100ps
 
 	/* 模組名稱 | Module Name
@@ -9,13 +11,13 @@
 		 著作權宣告 | Copyright Declaration
 				copyright 2012 林博仁(pika1021@gmail.com) */
 	module ();
-	//port 輸出輸入宣告
-	output ;
-	input ;
+		//port 輸出輸入宣告
+		output ;
+		input ;
 
-	//port 類型宣告
-	reg ;
-	wire ;
+		//port 類型宣告
+		reg ;
+		wire ;
 
 	endmodule
 `endif
