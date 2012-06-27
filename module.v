0f5f786260039cd46c4db278aeb3a5e76902e093
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-06-27 15:35:48 +0800

    [module]合併port宣告

--- module.v
+++ module.v
@@ -1,23 +1,19 @@
 //include guard
-`ifndef 
-	`define 
+`ifndef
+	`define
 	// 指定模擬器的時間單位與精確度
 	// 參考資料 | Reference Data
 	// 		http://www.asic-world.com/verilog/compiler2.html#%60timescale
 	`timescale 1ns / 100ps
 
 	/* 模組名稱 | Module Name
-				
+
 		 著作權宣告 | Copyright Declaration
 				copyright 2012 林博仁(pika1021@gmail.com) */
 	module ();
-		//port 輸出輸入宣告
+		//port 類型宣告
 		output ;
 		input ;
-
-		//port 類型宣告
-		reg ;
-		wire ;
 
 	endmodule
 `endif
